27f06927456a1096f8202a62c286bb1fa3939361
Adam-Elmi <Adamcade123@gmail.com>
2025-01-24 12:00:35 +0300

    Add obsidian-path.lua

---
+++ obsidian-path.lua
@@ -0,0 +1,14 @@
+local get_file_path = require("get-file-path")
+local color = require("colorize")
+colorize = color.colorize
+user_input = get_file_path
+
+local file = io.open("config.txt", "w")
+
+if file then
+    local path = user_input()
+    print(colorize(("Current vault: " .. path), "yellow"))
+    file:write(path)
+end
+
+file:close()
