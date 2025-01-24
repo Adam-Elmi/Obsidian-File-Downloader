03ee665db1a3e2f036abb51d2340162c3b99d810
Adam-Elmi <Adamcade123@gmail.com>
2025-01-24 12:00:35 +0300

    Add colorize.lua

---
+++ colorize.lua
@@ -0,0 +1,18 @@
+local color = {}
+
+function color.colorize(text, color)
+    local colors = {
+        reset = "\27[0m",
+        red = "\27[31m",
+        green = "\27[32m",
+        yellow = "\27[33m",
+        blue = "\27[34m",
+        magenta = "\27[35m",
+        cyan = "\27[36m",
+        white = "\27[37m",
+    }
+
+    return (colors[color] or colors.reset) .. text .. colors.reset
+end
+
+return color
