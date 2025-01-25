bbb8b875f075a88fc215840bb542ba71d227a320
Adam-Elmi <Adamcade123@gmail.com>
2025-01-25 23:45:28 +0300

    Modified utils.lua

--- utils.lua
+++ utils.lua
@@ -17,7 +17,7 @@ function utils.showPath()
     end
 end
 
-function utils.includes(table, value)
+function utils.command_includes(table, value)
     for _, v in pairs(table) do
         if v.command == value then
             return true
@@ -25,5 +25,40 @@ function utils.includes(table, value)
     end
     return false
 end
+
+local function getFileExtension(filename)
+    return filename:match("%.(%w+)$")
+end
+
+local function scanFolder()
+    local f = io.open("config.txt", "r")
+    if not f then
+        print("Failed to open config.txt")
+        return
+    end
+
+    local path = f:read("*l")
+    f:close()
+
+    if not path then
+        print("No path found in config.txt")
+        return
+    end
+
+    local handle = io.popen('ls "' .. path .. '"')
+    if not handle then
+        print("Failed to list folder contents")
+        return
+    end
+
+    for file in handle:lines() do
+        local fullPath = path .. "/" .. file
+        print(fullPath)
+    end
+
+    handle:close()
+end
+
+scanFolder()
 
 return utils
