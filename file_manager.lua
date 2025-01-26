ea8912291435cda76aa63d1a2369783814900c50
Adam-Elmi <Adamcade123@gmail.com>
2025-01-26 12:40:51 +0300

    Updated file_manager.lua

--- file_manager.lua
+++ file_manager.lua
@@ -3,7 +3,32 @@ local utils = require("utils")
 
 function file_manager()
     local file = io.open("config.txt", "r")
-    local path = file:read("*a")
+    local path = file:read("*a"):gsub("%s+", "")
+
+    local repo = ""
+
+    function download()
+        print("Enter the repository URL to download:\n")
+        io.write("> ")
+        repo = io.read()
+        print("Downloading repository:", repo)
+        local handle = io.popen("cd " .. path .. " && git clone " .. repo)
+        handle:close()
+
+        local repo_name = repo:match("/([^/]+)%.git$")
+        local repoPath = path .. "/" .. repo_name
+        local handleRepo = io.popen(
+            "cd " .. repoPath .. 
+            ' && find . -type f -not -name "*.md" -delete' .. 
+            ' && find . -type f -name "*.md" -exec mv {} ' .. path .. ' \\;' ..
+            ' && rm -r ' .. repoPath
+        )
+        local result = handleRepo:read("*a")
+        print(result)
+        handleRepo:close()
+        
+    end
+
     local commands = {
         {
             command = "target_folder -c",
@@ -12,6 +37,10 @@ function file_manager()
         {
             command = "showPath",
             action = utils.showPath
+        },
+        {
+            command = "dw",
+            action = download
         }
     }
      -- command input
